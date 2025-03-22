2e8d1ea03e041c8f5e050d4e7757e0f583e08b31
ABHIJITH T <24mp2275@rit.ac.in>
2025-03-22 11:50:54 +0530

    Create day-1.js

---
+++ NOTE/day-1.js
@@ -0,0 +1,9 @@
+variable and constant
+  <> A "data container" / "data storage"
+  <> Where the value can change!
+      let
+      var
+      const
+  <> Use constant as often as possible 
+  <> Whenever you actually got data that never changes
+  <> To be clear about your intentions (in your code).
